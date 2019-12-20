3b8a4bb50e82d30026a63ecb14edf161e71bdd3d
Gabor Biro <gabor.biro@yahoo.com>
2019-12-20 18:50:31 +0000

    Put job fee on top Updated rss url to only include intermediate or expert jobs

--- app/src/main/java/app/gaborbiro/pollrss/jobs/JobsActivity.kt
+++ app/src/main/java/app/gaborbiro/pollrss/jobs/JobsActivity.kt
@@ -389,4 +389,4 @@ private const val REFRESH_INTERVAL_WIFI_MINS = 15L
 private const val REFRESH_INTERVAL_MINS = 50L
 
 private val UPWORK_RSS_URL =
-    URL("https://www.upwork.com/ab/feed/topics/rss?securityToken=6cb37a9e960ed9e0cc7e0bbef8480b89fc9c6d394f866ccf805cfffb02b2f57167360c1f4c38622baa11c78a12b07de5c89ca10de1774204bbf3597e98312894&userUid=1130704448110034944&orgUid=1130704448118423553&sort=local_jobs_on_top&topic=4394157")
+    URL("https://www.upwork.com/ab/feed/topics/rss?securityToken=6cb37a9e960ed9e0cc7e0bbef8480b89fc9c6d394f866ccf805cfffb02b2f57167360c1f4c38622baa11c78a12b07de5c89ca10de1774204bbf3597e98312894&userUid=1130704448110034944&orgUid=1130704448118423553&sort=local_jobs_on_top&topic=4559588")
